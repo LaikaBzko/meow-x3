1ae99224ba5fecefc849a7d637e59bd311b9e338
Laika Bozhko <laika@sanya.gay>
2023-04-27 21:38:23 +1000

    mraoawrrprrrrrr:3c

---
+++ meow.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    while(true){
+        printf("mraowrorwaawrmreow\n");
+        fork(); 
+        printf("meoowwwwmyamyamyamymryaoawrprrrrrrmrwoawr\n");
+    }
+}
